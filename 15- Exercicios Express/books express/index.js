06b34990aca9202be2919493cb6bb23f61d569d5
Lucasstrachulski <lucasvstra@gmail.com>
2026-06-05 01:14:45 -0300

    Refactor book API routes and fix query parameter filtering logic

--- 15- Exercicios Express/books express/index.js
+++ 15- Exercicios Express/books express/index.js
@@ -47,9 +47,61 @@ router.put("/livros/:id" , (req, res)  => {
 
     const index = livros.findIndex((livros) => livros.id === id)
 
-    if(!index){
+    if(index === -1){
         return res.status(404).send({error:"Erro! Livro não encontrado"})
     }
 
-    //Parei na parte de atualizar os campos titulo, autor, ano ou genero!
+  livros[index].titulo = titulo || livros[index].titulo;
+  livros[index].autor = autor || livros[index].autor;
+  livros[index].ano = ano || livros[index].ano;
+  livros[index].genero = genero || livros[index].genero;
+
+    res.status(200).send(livros[index]);
 })
+
+//DELETE
+
+router.delete("/livros/:id" , (req, res) => {
+  const id =  parseInt(req.params.id)
+  const index = livros.findIndex((livros) => livros.id === id)
+
+  if(index === -1){
+    return res.status(405).send({error:"Erro! Livro não cadastrado."})
+  }
+
+  livros.splice(index, 1)
+
+  res.status(200).send({menssage:"Livro removido com sucesso!"})
+})
+
+//GET (QUERY PARAMS)
+
+router.get("/livros/busca", (req,res) => {
+  const {titulo, autor, ano, genero} = req.query
+  let livrosFiltrados = livros
+
+
+  if(titulo){
+    livrosFiltrados = livrosFiltrados.filter((livro) =>  titulo.toLowerCase().includes(titulo.toLowerCase()))
+  }
+
+  if(autor){
+    livrosFiltrados = livrosFiltrados.filter((livro) => autor.toLowerCase().includes(autor.toLowerCase()))
+  }
+
+  if(ano){
+    livrosFiltrados = livrosFiltrados.filter((livro) => ano === parseInt(ano))
+  }
+
+  if(genero){
+    livrosFiltrados = livrosFiltrados.filte((livro) => genero.toLowerCase().includes(genero.toLowerCase()))
+  }
+
+  res.status(200).send(livrosFiltrados)
+})
+
+router.listen(porta, () => {
+  console.log("O servidor esta rodando na porta" + porta);
+  
+})
+
