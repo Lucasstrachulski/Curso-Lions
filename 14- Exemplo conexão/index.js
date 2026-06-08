e53e2764b2ca701be734736ea0a9c69529b2b630
Lucasstrachulski <lucasvstra@gmail.com>
2026-06-08 03:14:08 -0300

    setup express server and database connection

--- 14- Exemplo conexão/index.js
+++ 14- Exemplo conexão/index.js
@@ -1,6 +1,62 @@
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import express from "express";
+import VendaMensal from "./VendaMensal.js";
 
 dotenv.config();
 
-await mongoose.connect(process.env.MONGO_URI);
+const app = express()
+const porta = 3000;
+
+app.use(express.json())
+
+const connectDB = async () => {
+
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+   console.log(`Conectado ao Mongo DB`);
+    } catch (error) {
+         console.log("Erro ao conectar com o  Mongo DB", error);
+    }
+}
+
+connectDB()
+
+
+
+//CREATE 
+ app.post("/vendas", async (req,res) => {
+    try {
+         const novaVendaMensal = await VendaMensal.create(req.body)
+    res.json(novaVendaMensal)
+    } catch (error) {
+        res.send(error)
+    }
+   
+ })
+
+ //READ
+ app.get("/vendas",  async (req,res) => {
+try {
+    const vendasMensais = await VendaMensal.find()
+      res.json(novaVendaMensal)
+} catch (error) {
+          res.send(error)
+}
+})
+
+//UPDATE
+
+ app.put("/vendas/:id",  async (req,res) => {
+    //req.params.id
+try {
+    const novaVendaMensal = await VendaMensal.findByIdAndUpdate(req.params.id, req.body, {ney: true});
+      res.json(novaVendaMensal)
+} catch (error) {
+          res.send(error)
+}
+})
+
+
+app.listen(porta, () => console.log(`O servidor está rodando na porta ${porta}`)
+)
