556b5c9fb45da9d7b773bdff3c75e47f62f9c55f
Lucasstrachulski <lucasvstra@gmail.com>
2026-06-08 03:16:51 -0300

    setup express server and database connection

--- 14- Exemplo conexão/index.js
+++ 14- Exemplo conexão/index.js
@@ -5,58 +5,68 @@ import VendaMensal from "./VendaMensal.js";
 
 dotenv.config();
 
-const app = express()
+const app = express();
 const porta = 3000;
 
-app.use(express.json())
+app.use(express.json());
 
 const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log(`Conectado ao Mongo DB`);
+  } catch (error) {
+    console.log("Erro ao conectar com o  Mongo DB", error);
+  }
+};
 
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-   console.log(`Conectado ao Mongo DB`);
-    } catch (error) {
-         console.log("Erro ao conectar com o  Mongo DB", error);
-    }
-}
+connectDB();
 
-connectDB()
+//CREATE
+app.post("/vendas", async (req, res) => {
+  try {
+    const novaVendaMensal = await VendaMensal.create(req.body);
+    res.json(novaVendaMensal);
+  } catch (error) {
+    res.send(error);
+  }
+});
 
-
-
-//CREATE 
- app.post("/vendas", async (req,res) => {
-    try {
-         const novaVendaMensal = await VendaMensal.create(req.body)
-    res.json(novaVendaMensal)
-    } catch (error) {
-        res.send(error)
-    }
-   
- })
-
- //READ
- app.get("/vendas",  async (req,res) => {
-try {
-    const vendasMensais = await VendaMensal.find()
-      res.json(novaVendaMensal)
-} catch (error) {
-          res.send(error)
-}
-})
+//READ
+app.get("/vendas", async (req, res) => {
+  try {
+    const vendasMensais = await VendaMensal.find();
+    res.json(novaVendaMensal);
+  } catch (error) {
+    res.send(error);
+  }
+});
 
 //UPDATE
 
- app.put("/vendas/:id",  async (req,res) => {
-    //req.params.id
-try {
-    const novaVendaMensal = await VendaMensal.findByIdAndUpdate(req.params.id, req.body, {ney: true});
-      res.json(novaVendaMensal)
-} catch (error) {
-          res.send(error)
-}
-})
+app.put("/vendas/:id", async (req, res) => {
+  try {
+    const novaVendaMensal = await VendaMensal.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { ney: true },
+    );
+    res.json(novaVendaMensal);
+  } catch (error) {
+    res.send(error);
+  }
+});
 
+//DELETE
 
-app.listen(porta, () => console.log(`O servidor está rodando na porta ${porta}`)
-)
+app.delete("/vendas/:id", async (req, res) => {
+  try {
+    const VendaExcluida = await VendaMensal.findByIdAndDelete(req.params.id);
+    res.json(VendaExcluida);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+app.listen(porta, () =>
+  console.log(`O servidor está rodando na porta ${porta}`),
+);
